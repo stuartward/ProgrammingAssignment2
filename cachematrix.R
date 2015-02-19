85f65a09ef420d9b553e01418addd38984670811
stuartward <stuart@stuartward.com>
2015-02-19 13:12:48 -0500

    Final commit of solution to Programming Assignment 2
    This is the final commit of the solution to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -22,11 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-
 ## The function below (cacheSolve) computes the inverse of the special "matrix"
 ## returned by makeCacheMatrix function above.
 ## If the inverse has already been calculated (and the matrix has not changed),
-##then the cachesolve should retrieve the inverse from the cache.
+## then the cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -40,24 +39,3 @@ cacheSolve <- function(x, ...) {
         x$setsolve(m)
         m
 }
-
-
-# # Code to test the functions
-# 
-# # Create test matrix
-# b <- matrix(c(2, 4, 5, 7), nrow=2, ncol=2)
-# 
-# # View test matrix
-# b
-# 
-# # Take the inverse of the text matrix using the solve function
-# solve(b)
-# 
-# # Set x equal to results of makeCacheMatrix; passing in test matrix
-# x <- makeCacheMatrix(matrix(c(2, 4, 5, 7), nrow=2, ncol=2))
-# 
-# # First time calling cacheSolve; returns the inverse
-# cacheSolve(x)
-# 
-# # Second time calling cacheSolve; returns the inverse from cache
-# cacheSolve(x)
