15cd3fcbe5e7b788183f92472e60b8a1431473e8
stuartward <stuart@stuartward.com>
2015-02-19 11:25:07 -0500

    Test commit 01
    Test commit 01

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Test comment for test commit
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
